02601a2077831397cd5cb39264af63e057470513
Ollitros <Ollitros@gmail.com>
2019-01-27 11:58:29 +0200

    MobileNet module

--- FinalPractice/Inception/inception.py
+++ FinalPractice/Inception/inception.py
@@ -7,15 +7,34 @@ from tensorflow.keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPool2D, 
 start_time = time.time()
 
 
-def conv_block(inputs, filters, filter_size, strides=(1, 1)):
-    x = Conv2D(filters, filter_size, strides=strides, padding='same')(inputs)
+def conv_block(inputs, filters, filter_size,  alpha, strides=(1, 1)):
+    x = Conv2D(int(filters*alpha), filter_size, strides=strides, padding='same')(inputs)
     x = BatchNormalization()(x)
     x = LeakyReLU()(x)
 
     return x
 
 
-def InceptionNN(input_shape):
+def inception_block(x, channel_axis, alpha):
+    conv1x1 = conv_block(x, 64, (1, 1), alpha=alpha)
+    conv5x5 = conv_block(x, 48, (1, 1), alpha=alpha)
+    conv5x5 = conv_block(conv5x5, 64, (5, 5), alpha=alpha)
+
+    conv3x3 = conv_block(x, 64, (1, 1), alpha=alpha)
+    conv3x3 = conv_block(conv3x3, 96, (3, 3), alpha=alpha)
+    conv3x3 = conv_block(conv3x3, 96, (3, 3), alpha=alpha)
+
+    branch_pool = AveragePooling2D((3, 3), strides=(1, 1), padding='same')(x)
+    branch_pool = conv_block(branch_pool, 32, (1, 1), alpha=alpha)
+
+    x = concatenate([conv1x1, conv5x5, conv3x3, branch_pool], axis=channel_axis)
+    x = MaxPool2D((3, 3), strides=(1, 1))(x)
+    x = Dropout(0.25)(x)
+
+    return x
+
+
+def InceptionNN(input_shape, alpha=1.0):
     if tf.keras.backend.image_data_format() == 'channels_first':
         channel_axis = 1
     else:
@@ -23,96 +42,31 @@ def InceptionNN(input_shape):
 
     # Preliminary convolution
     inputs = Input(shape=input_shape)
-    x = conv_block(inputs, 32, (3, 3))
-    x = conv_block(x, 32, (3, 3))
-    x = conv_block(x, 64, (3, 3))
+    x = conv_block(inputs, 32, (3, 3), alpha=alpha)
+    x = conv_block(x, 32, (3, 3), alpha=alpha)
+    x = conv_block(x, 64, (3, 3), alpha=alpha)
     x = MaxPool2D((3, 3), strides=(2, 2))(x)
     x = Dropout(0.25)(x)
-    x = conv_block(x, 80, (1, 1))
-    x = conv_block(x, 192,(3, 3))
+    x = conv_block(x, 80, (1, 1), alpha=alpha)
+    x = conv_block(x, 192, (3, 3), alpha=alpha)
     x = MaxPool2D((3, 3), strides=(1, 1))(x)
     x = Dropout(0.25)(x)
 
     # Inception blocks
     # Inception block 1
-    conv1x1 = conv_block(x, 64, (1, 1))
-    conv5x5 = conv_block(x, 48, (1, 1))
-    conv5x5 = conv_block(conv5x5, 64, (5, 5))
-
-    conv3x3 = conv_block(x, 64, (1, 1))
-    conv3x3 = conv_block(conv3x3, 96, (3, 3))
-    conv3x3 = conv_block(conv3x3, 96, (3, 3))
-
-    branch_pool = AveragePooling2D((3, 3), strides=(1, 1), padding='same')(x)
-    branch_pool = conv_block(branch_pool, 32, 1, 1)
-
-    x = concatenate([conv1x1, conv5x5, conv3x3, branch_pool], axis=channel_axis, name='inception_block_1')
-    x = MaxPool2D((3, 3), strides=(1, 1))(x)
-    x = Dropout(0.25)(x)
+    x = inception_block(x, channel_axis=channel_axis, alpha=alpha)
 
     # Inception block 2
-    conv1x1 = conv_block(x, 64, (1, 1))
-    conv5x5 = conv_block(x, 48, (1, 1))
-    conv5x5 = conv_block(conv5x5, 64, (5, 5))
-
-    conv3x3 = conv_block(x, 64, (1, 1))
-    conv3x3 = conv_block(conv3x3, 96, (3, 3))
-    conv3x3 = conv_block(conv3x3, 96, (3, 3))
-
-    branch_pool = AveragePooling2D((3, 3), strides=(1, 1), padding='same')(x)
-    branch_pool = conv_block(branch_pool, 32, 1, 1)
-
-    x = concatenate([conv1x1, conv5x5, conv3x3, branch_pool], axis=channel_axis, name='inception_block_2')
-    x = MaxPool2D((3, 3), strides=(1, 1))(x)
-    x = Dropout(0.25)(x)
+    x = inception_block(x, channel_axis=channel_axis, alpha=alpha)
 
     # Inception block 3
-    conv1x1 = conv_block(x, 64, (1, 1))
-    conv5x5 = conv_block(x, 48, (1, 1))
-    conv5x5 = conv_block(conv5x5, 64, (5, 5))
-
-    conv3x3 = conv_block(x, 64, (1, 1))
-    conv3x3 = conv_block(conv3x3, 96, (3, 3))
-    conv3x3 = conv_block(conv3x3, 96, (3, 3))
-
-    branch_pool = AveragePooling2D((3, 3), strides=(1, 1), padding='same')(x)
-    branch_pool = conv_block(branch_pool, 32, 1, 1)
-
-    x = concatenate([conv1x1, conv5x5, conv3x3, branch_pool], axis=channel_axis, name='inception_block_3')
-    x = MaxPool2D((3, 3), strides=(1, 1))(x)
-    x = Dropout(0.25)(x)
+    x = inception_block(x, channel_axis=channel_axis, alpha=alpha)
 
     # Inception block 4
-    conv1x1 = conv_block(x, 64, (1, 1))
-    conv5x5 = conv_block(x, 48, (1, 1))
-    conv5x5 = conv_block(conv5x5, 64, (5, 5))
-
-    conv3x3 = conv_block(x, 64, (1, 1))
-    conv3x3 = conv_block(conv3x3, 96, (3, 3))
-    conv3x3 = conv_block(conv3x3, 96, (3, 3))
-
-    branch_pool = AveragePooling2D((3, 3), strides=(1, 1), padding='same')(x)
-    branch_pool = conv_block(branch_pool, 32, 1, 1)
-
-    x = concatenate([conv1x1, conv5x5, conv3x3, branch_pool], axis=channel_axis, name='inception_block_4')
-    x = MaxPool2D((3, 3), strides=(1, 1))(x)
-    x = Dropout(0.25)(x)
+    x = inception_block(x, channel_axis=channel_axis, alpha=alpha)
 
     # Inception block 5
-    conv1x1 = conv_block(x, 64, (1, 1))
-    conv5x5 = conv_block(x, 48, (1, 1))
-    conv5x5 = conv_block(conv5x5, 64, (5, 5))
-
-    conv3x3 = conv_block(x, 64, (1, 1))
-    conv3x3 = conv_block(conv3x3, 96, (3, 3))
-    conv3x3 = conv_block(conv3x3, 96, (3, 3))
-
-    branch_pool = AveragePooling2D((3, 3), strides=(1, 1), padding='same')(x)
-    branch_pool = conv_block(branch_pool, 32, 1, 1)
-
-    x = concatenate([conv1x1, conv5x5, conv3x3, branch_pool], axis=channel_axis, name='inception_block_5')
-    x = MaxPool2D((3, 3), strides=(1, 1))(x)
-    x = Dropout(0.25)(x)
+    x = inception_block(x, channel_axis=channel_axis, alpha=alpha)
 
     # Final part
     x = GlobalAveragePooling2D()(x)
@@ -130,12 +84,13 @@ def InceptionNN(input_shape):
 num_classes = 10
 batch_size = 300
 epochs = 5
+alpha = 1.0
 
 y_train = tf.keras.utils.to_categorical(y_train, num_classes=num_classes)
 y_test = tf.keras.utils.to_categorical(y_test, num_classes=num_classes)
 input_shape = (32, 32, 3)
 
-model = InceptionNN(input_shape=input_shape)
+model = InceptionNN(input_shape=input_shape, alpha=alpha)
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 print(model.summary())
 model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs)
---
+++ FinalPractice/MobileNets/mobile_net.py
@@ -0,0 +1,91 @@
+import tensorflow as tf
+import time
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPool2D, \
+    Input, BatchNormalization, LeakyReLU, GlobalAveragePooling2D, AveragePooling2D, DepthwiseConv2D
+
+start_time = time.time()
+
+
+def conv_block(inputs, filters, filter_size,  alpha, strides=(1, 1)):
+    x = Conv2D(int(filters*alpha), filter_size, strides=strides, padding='same')(inputs)
+    x = BatchNormalization()(x)
+    x = LeakyReLU()(x)
+
+    return x
+
+
+def depthwise_block(x, filters, alpha, channel_axis):
+
+    x = DepthwiseConv2D((3, 3), padding='same')(x)
+    x = BatchNormalization(axis=channel_axis)(x)
+    x = LeakyReLU()(x)
+
+    x = Conv2D(int(alpha*filters), (1, 1), padding='same')(x)
+    x = BatchNormalization(axis=channel_axis)(x)
+    x = LeakyReLU()(x)
+
+    return x
+
+
+def MobileNet(input_shape, alpha=1.0):
+    if tf.keras.backend.image_data_format() == 'channels_first':
+        channel_axis = 1
+    else:
+        channel_axis = 3
+
+    # Preliminary convolution
+    inputs = Input(shape=input_shape)
+    x = conv_block(inputs, 32, (3, 3), alpha=alpha)
+
+    # Depthwise blocks
+    x = depthwise_block(x, 64,  alpha=alpha, channel_axis=channel_axis)
+    x = depthwise_block(x, 128, alpha=alpha, channel_axis=channel_axis)
+    x = Dropout(0.25)(x)
+    x = MaxPool2D(strides=(2, 2))(x)
+
+    x = depthwise_block(x, 128, alpha=alpha, channel_axis=channel_axis)
+    x = depthwise_block(x, 256, alpha=alpha, channel_axis=channel_axis)
+    x = Dropout(0.25)(x)
+    x = MaxPool2D(strides=(2, 2))(x)
+
+    x = depthwise_block(x, 256, alpha=alpha, channel_axis=channel_axis)
+    x = depthwise_block(x, 512, alpha=alpha, channel_axis=channel_axis)
+    x = Dropout(0.25)(x)
+    x = MaxPool2D(strides=(2, 2))(x)
+
+    x = depthwise_block(x, 512, alpha=alpha, channel_axis=channel_axis)
+    x = depthwise_block(x, 512, alpha=alpha, channel_axis=channel_axis)
+    x = Dropout(0.25)(x)
+    x = MaxPool2D(strides=(2, 2))(x)
+
+    # Final part
+    x = GlobalAveragePooling2D()(x)
+    x = Flatten()(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dropout(0.25)(x)
+    dense = Dense(10, activation='softmax')(x)
+
+    model = tf.keras.models.Model(inputs=[inputs], outputs=[dense])
+
+    return model
+
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+num_classes = 10
+batch_size = 300
+epochs = 5
+
+y_train = tf.keras.utils.to_categorical(y_train, num_classes=num_classes)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes=num_classes)
+input_shape = (32, 32, 3)
+
+model = MobileNet(input_shape=input_shape, alpha=1.0)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+print(model.summary())
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs)
+
+score = model.evaluate(x_test, y_test)
+print(score)
+
+print("\n\n\nThe program has been finished for --- %s seconds ---" % (time.time() - start_time))
