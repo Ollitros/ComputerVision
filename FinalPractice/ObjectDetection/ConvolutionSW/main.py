9ba05a59c7cf979c60c8f3401253c39173e775a6
Ollitros <Ollitros@gmail.com>
2019-02-11 11:43:29 +0200

    Completed creating own dataset

---
+++ FinalPractice/ObjectDetection/ConvolutionSW/main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+train_x = np.load("../data/labeled/train.npy")
+train_y = pd.read_csv("../data/labeled/train_labels.csv")
+
---
+++ FinalPractice/ObjectDetection/README.txt
@@ -0,0 +1,3 @@
+How to create dataset?
+Find images -> use labelImg.exe -> using utils.py transform xml format of features and labels to csv.
+Use example in data folder.
