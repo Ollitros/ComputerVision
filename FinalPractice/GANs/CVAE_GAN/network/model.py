7e8460da532c4a340d9b89ee3713dd64f885c179
Ollitros <Ollitros@gmail.com>
2019-05-30 19:59:56 +0300

    VAE-CGAN for 3D mnist has benn started

--- FinalPractice/GANs/CVAE_GAN/network/model.py
+++ FinalPractice/GANs/CVAE_GAN/network/model.py
@@ -21,12 +21,27 @@ class Gan:
         self.discriminator = self.build_discriminator()
 
         # Build the generator
-        self.generator = self.build_generator()
+        self.encoder, self.decoder = self.build_generator()
+
+        self.encoder_input = self.encoder.inputs
+        self.encode = self.encoder(self.encoder_input)
+
+        # Create generators
+        self.decode = self.decoder(self.encode)
+        self.generator = Model(self.encoder_input, self.decode)
+        self.encoder.summary()
+        self.decoder.summary()
+        self.generator.summary()
 
         # Define variables
         self.distorted, self.fake, self.condition = self.define_variables(generator=self.generator)
 
         self.real = Input(shape=self.img_shape)
+
+    def sampling(self, args):
+        z_mean, z_log_sigma = args
+        epsilon = K.random_normal(shape=(4 * 4 * 128,), mean=0, stddev=1)
+        return z_mean + K.exp(z_log_sigma / 2) * epsilon
 
     def build_generator(self):
 
@@ -35,22 +50,61 @@ class Gan:
         label_embedding = Flatten()(Embedding(self.num_classes, self.latent_dim)(label))
         model_input = multiply([noise, label_embedding])
 
+        # # #######################
+        # # ## Build encoder
+        # # #######################
         x = Reshape((32, 32, 3))(model_input)
-        x = conv_block(128, strides=(1, 1))(x)
-        x = conv_block(256, strides=(1, 1))(x)
-        x = conv_block(256, strides=(1, 1))(x)
-        x = conv_block(128, strides=(1, 1))(x)
-        x = conv_block(64, strides=(1, 1))(x)
-        x = conv_block(32, strides=(1, 1))(x)
+        x = Conv2D(32, kernel_size=5, use_bias=False, padding="same")(x)
+        x = conv_block(64)(x)
+        x = self_attn_block(x, 64)
+        x = conv_block(64)(x)
+        x = self_attn_block(x, 64)
+        x = conv_block(64)(x)
+
+        activ_map_size = self.img_shape[0] // 16
+        while activ_map_size > 4:
+            x = conv_block(64)(x)
+            activ_map_size = activ_map_size // 2
+
+        x = Dense(128)(Flatten()(x))
+        x = Dense(4 * 4 * 128)(x)
+
+        # Latent Variable Calculation
+        z_mean = BatchNormalization()(x)
+        dense_2 = Dense(4 * 4 * 128, name='z_log_sigma')(z_mean)
+        z_log_sigma = BatchNormalization()(dense_2)
+        encoder_output = Lambda(self.sampling)([z_mean, z_log_sigma])
+
+        encoder = Model(inputs=[noise, label], outputs=encoder_output)
+
+        # # #######################
+        # # ## Build decoder
+        # # #######################
+
+        decoder_input = Input(shape=(4 * 4 * 128, ))
+        x = Reshape((4, 4, 128))(decoder_input)
+        x = upscale(64)(x)
+        x = upscale(64)(x)
+        x = self_attn_block(x, 64)
+        x = upscale(64)(x)
+        x = res_block(x, 64)
+        x = self_attn_block(x, 64)
+
+        outputs = []
+        activ_map_size = self.img_shape[0] * 8
+        while activ_map_size < 128:
+            outputs.append(Conv2D(3, kernel_size=5, padding='same', activation="tanh")(x))
+            x = upscale(64)(x)
+            x = conv_block(64, strides=1)(x)
+            activ_map_size *= 2
 
         alpha = Conv2D(1, kernel_size=3, padding='same', activation="sigmoid")(x)
         bgr = Conv2D(3, kernel_size=3, padding='same', activation="tanh")(x)
         x = concatenate([alpha, bgr])
 
-        model = Model([noise, label], x)
-        model.summary()
+        decoder = Model(inputs=decoder_input, outputs=x)
 
-        return model
+        return encoder, decoder
 
     def build_discriminator(self):
 
@@ -95,8 +149,6 @@ class Gan:
         weights['w_D'] = 0.1  # Discriminator
         weights['w_recon'] = 1.  # L1 reconstruction loss
         weights['w_edge'] = 0.1  # edge loss
-        weights['w_eyes'] = 30.  # reconstruction and edge loss on eyes area
-        weights['w_pl'] = (0.01, 0.1, 0.3, 0.1)  # perceptual loss (0.003, 0.03, 0.3, 0.3)
 
         # Adversarial loss
         loss_dis, loss_adv_gen = adversarial_loss(self.discriminator, self.real,  self.fake, self.distorted, self.condition)
--- FinalPractice/GANs/CVAE_GAN/train_model.py
+++ FinalPractice/GANs/CVAE_GAN/train_model.py
@@ -1,22 +1,52 @@
 import numpy as np
 import cv2 as cv
 import time
-from keras.datasets import cifar10
+from keras.datasets import cifar10, mnist
 from FinalPractice.GANs.CVAE_GAN.network.model import Gan
 
 
 def sample_images(model, epoch):
 
-    sampled_labels = np.arange(0, 10).reshape(-1, 1)
+    # sampled_labels = np.arange(0, 10).reshape(-1, 1)
+    #
+    # batch_noise = np.random.normal(0, 1, (10, 32 * 32 * 3))
+    # prediction = model.generator.predict([batch_noise, sampled_labels])
+    # prediction = np.float32(prediction * 255)[:, :, :, 1:4]
+    #
+    # image = cv.hconcat([prediction[0], prediction[1], prediction[2], prediction[3], prediction[4],
+    #                     prediction[5],  prediction[6], prediction[7], prediction[8], prediction[9]])
+    #
+    # cv.imwrite('data/images/{step}.jpg'.format(step=epoch), image)
 
-    batch_noise = np.random.normal(0, 1, (10, 32 * 32 * 3))
-    prediction = model.generator.predict([batch_noise, sampled_labels])
+    batch_noise = np.random.rand(10, 4 * 4 * 128)
+    prediction = model.decoder.predict(batch_noise)
     prediction = np.float32(prediction * 255)[:, :, :, 1:4]
 
     image = cv.hconcat([prediction[0], prediction[1], prediction[2], prediction[3], prediction[4],
                         prediction[5],  prediction[6], prediction[7], prediction[8], prediction[9]])
 
     cv.imwrite('data/images/{step}.jpg'.format(step=epoch), image)
+
+
+def test(x, y):
+
+    model = Gan()
+    model.build_train_functions()
+    model.load_weights()
+
+    for i in range(100):
+
+        batch_noise = np.random.normal(0, 1, (1, 32 * 32 * 3))
+        # prediction = model.generator.predict([batch_noise, np.asarray([y[i]])])
+        prediction = model.generator.predict([batch_noise, np.asarray([5])])
+        prediction = np.float32(prediction * 255)[:, :, :, 1:4]
+
+        print(y[i])
+        print(prediction.shape)
+        cv.imwrite('data/{i}.jpg'.format(i=i), np.reshape(prediction, (32, 32, 3)))
+        cv.imshow('asdasd', np.reshape(prediction, (32, 32, 3)))
+        cv.waitKey(0)
+        cv.destroyAllWindows()
 
 
 def train_model(input_shape, x, y, epochs, batch_size):
@@ -28,7 +58,7 @@ def train_model(input_shape, x, y, epochs, batch_size):
     display_iters = 1
 
     t0 = time.time()
-    model.load_weights()
+    # model.load_weights()
 
     iters = np.asarray(x).shape[0] // batch_size
     if np.asarray(x).shape[0] - iters * batch_size == 0:
@@ -37,17 +67,11 @@ def train_model(input_shape, x, y, epochs, batch_size):
     train_x = np.asarray(x)
     for epoch in range(epochs):
 
-        # Train generator predict first frame form noise
-        # Train discriminator
-        batch_y = []
-        for f in range(batch_size):
-            batch_y.append(train_x[0])
-
         # Train discriminator
         step = 0
         for iter in range(iters):
             batch_noise = np.random.normal(0, 1, (batch_size, input_shape[0] * input_shape[1] * input_shape[2]))
-            fake = model.generator.predict([batch_noise, y])
+            fake = model.generator.predict([batch_noise, y[step: (step + batch_size)]])
             fake = np.reshape(fake[:, :, :, 1:4], (batch_size, input_shape[0] * input_shape[1] * input_shape[2]))
             errD = model.train_discriminator(X=fake, Y=train_x[step: (step + batch_size)], cond=y[step: (step + batch_size)])
             step = step + batch_size
@@ -81,6 +105,7 @@ def train_model(input_shape, x, y, epochs, batch_size):
 
         # Makes predictions after each epoch and save into temp folder.
         if epoch % 1 == 0:
+
             sample_images(model, epoch + 0)
             model.save_weights()
 
@@ -88,16 +113,30 @@ def train_model(input_shape, x, y, epochs, batch_size):
 def main():
 
     # Load the dataset
-    (x_train, y_train), (_, _) = cifar10.load_data()
+    (x_train, y_train), (_, _) = mnist.load_data()
+    input_shape = (32, 32, 3)
     print(x_train.shape)
 
-    x_train = x_train.astype('float32')
-    x_train /= 255
+    x_resized = []
+    for i in x_train:
+        x_resized.append(cv.resize(i, (32, 32)))
+
+    print(np.asarray(x_resized).shape)
+
+    x = np.random.normal(0, 1, (60000, 32, 32, 3))
+    x[:, :, :, 0] = x_resized
+    x[:, :, :, 1] = x_resized
+    x[:, :, :, 2] = x_resized
+    print(x.shape)
+
+    x = x.astype('float32')
+    x /= 255
 
     epochs = 10
     batch_size = 128
-    input_shape = (32, 32, 3)
-    train_model(input_shape=input_shape, x=x_train, y=y_train, epochs=epochs, batch_size=batch_size)
+
+    # test(x, y_train)
+    train_model(input_shape=input_shape, x=x, y=y_train, epochs=epochs, batch_size=batch_size)
 
 
 if __name__ == '__main__':
